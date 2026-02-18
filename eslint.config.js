b99912d960a3783907928ffbba12f0edf135bb8e
vinaykumaru2k3 <vk5022702@gmail.com>
2026-02-18 15:31:42 +0530

    updated ESlint config for jsx

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+export default [
+  { ignores: ['dist', 'node_modules'] },
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: {
+        browser: true,
+        node: true,
+        es6: true,
+      },
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-undef': 'warn',
+    },
+  },
+];
