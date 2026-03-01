8d28f1407410fda98b9cbb93ac1a69ae76fc5fc1
Vinay Kumar U <vk5022702@gmail.com>
2026-03-01 10:40:58 +0530

    style: Revert slate mapping to neutral for true black dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import colors from 'tailwindcss/colors';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -9,6 +11,9 @@ export default {
     extend: {
       fontFamily: {
         sans: ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        slate: colors.neutral,
       }
     },
   },
